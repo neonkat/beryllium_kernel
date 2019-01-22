c71aa1d95c680ebbc99c6d7a6b70e79d3adff4be
neonkat <neonkat297@gmail.com>
2019-01-23 00:57:04 +0530

    init: cleanup

---
+++ ak/ramdisk/init.infected.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+  echo 8 > /sys/module/cpu_boost/parameters/dynamic_stune_boost 
+  echo "0:0" > /sys/module/cpu_boost/parameters/input_boost_freq 
+  echo 150 > /sys/module/cpu_boost/parameters/input_boost_ms 
+  echo 0 > /dev/stune/top-app/schedtune.sched_boost 
+  echo 1 > /dev/stune/foreground/schedtune.sched_boost 
+  echo 1 > /dev/stune/background/schedtune.sched_boost 
+  echo 1 > /dev/stune/rt/schedtune.sched_boost 
+  echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 
+  echo 800 > /sys/module/cpu_boost/parameters/powerkey_input_boost_ms
+  echo "0:0 4:2803200" > /sys/module/cpu_boost/parameters/powerkey_input_boost_freq 
