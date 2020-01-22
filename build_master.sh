27eeb381fa24a7a69a79f03eeaad2e96fe49bf2d
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-22 10:30:23 +0900

    build_master: switch to AnyKernel3
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -9,12 +9,11 @@ VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
 
 if [ -e boot.img ] ; then
 	rm arter97-kernel-$VERSION.zip 2>/dev/null
+	cp boot.img arter97-kernel-$VERSION.img
 
-	# Pack AnyKernel2
+	# Pack AnyKernel3
 	rm -rf kernelzip
-	mkdir -p kernelzip/dtbs
-	cp arch/arm64/boot/Image.gz kernelzip/
-	find arch/arm64/boot -name '*.dtb' -exec cp {} kernelzip/dtbs/ \;
+	mkdir kernelzip
 	echo "
 kernel.string=arter97 kernel $(cat version) @ xda-developers
 do.devicecheck=1
@@ -23,12 +22,14 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=beryllium
 block=/dev/block/bootdevice/by-name/boot
-is_slot_device=auto
+is_slot_device=0
 ramdisk_compression=auto
 " > kernelzip/props
-	cp -rp ~/android/anykernel2/* kernelzip/
+	cp -rp ~/android/anykernel/* kernelzip/
+	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
 	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
 	rm arter97-kernel-$VERSION-tmp.zip
 	cd ..
